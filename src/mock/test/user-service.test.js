1907ed8a017abb26c12839a02d74612468202f72
HyeonJu <hhjc017@gmail.com>
2022-09-06 18:29:31 +0900

    feat : isLoggedin test 추가

--- src/mock/test/user-service.test.js
+++ src/mock/test/user-service.test.js
@@ -3,8 +3,10 @@ const StubUser = require('./stub-user')
 
 describe('user service', () => {
   let userService
+  let stubUser
+
   beforeEach(() => {
-    const stubUser = new StubUser()
+    stubUser = new StubUser()
     userService = new UserSercive(stubUser)
   })
 
@@ -19,5 +21,18 @@ describe('user service', () => {
     } catch (error) {
       expect(error).toBe(/login failed./)
     }
+  })
+
+  it('shoud not be called if isLoggedin is "true', async () => {
+    // expect는 mock 함수 또는 spy 함수 만을 인자로 받음
+    // 따라서 이럴 때에는 stub 대신 user login과 관련된 mock을 만드는 것이 나을 수 있음
+    const stubUserLogin = jest.spyOn(stubUser, 'login')
+
+    // isLoggedin을 직접 true로 바꾸는 대신,
+    // login 함수를 두번 호출하는 방법도 있음
+    userService.isLoggedin = true
+    userService.login('이름', '전화번호')
+
+    expect(stubUserLogin).not.toHaveBeenCalled()
   })
 })
--- src/mock/user-service.js
+++ src/mock/user-service.js
@@ -5,6 +5,7 @@ class UserSercive {
   }
 
   async login(id, password) {
+    // 로그인 상태가 아닐 때에만 동작하는지 확인하는 테스트가 빠졌음!
     if (!this.isLoggedin) {
       try {
         await this.user.login(id, password)
